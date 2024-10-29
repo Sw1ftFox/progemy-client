690b6108213cc5da66d6e74ca255176132932a2c
Sw1ftFox <v.suvorkin2017@gmail.com>
2024-10-29 20:39:07 +0300

    created hook useCarousel

--- src/features/carousel/ui/Сarousel.tsx
+++ src/features/carousel/ui/Сarousel.tsx
@@ -3,80 +3,28 @@ import Arrow from "../../../icons/carousel/Arrow";
 import Circle from "../../../icons/carousel/Circle";
 const styles = require("./Carousel.module.scss");
 import { slidesArray } from "../../../entities/slidesData";
+import useCarousel from "../../../services/useCarousel";
 
 export const Сarousel = () => {
-  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
-  const [autoPlayInterval, setAutoPlayInterval] =
-    useState<NodeJS.Timeout | null>(null);
   const totalSlides = slidesArray.length;
-
-  const pauseAutoPlay = () => {
-    if (autoPlayInterval) {
-      clearInterval(autoPlayInterval);
-      setAutoPlayInterval(null);
-    }
-  };
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentSlideIndex((prevIndex) => (prevIndex + 1) % totalSlides);
-    }, 7000);
-
-    setAutoPlayInterval(interval);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-
-  const handlePreviousSlide = () => {
-    pauseAutoPlay();
-    setCurrentSlideIndex(
-      (prevIndex) => (prevIndex - 1 + totalSlides) % totalSlides
-    );
-  };
-
-  const handleNextSlide = () => {
-    pauseAutoPlay();
-    setCurrentSlideIndex((prevIndex) => (prevIndex + 1) % totalSlides);
-  };
-
-  const handleGoToSlide = (index: number) => {
-    pauseAutoPlay();
-    setCurrentSlideIndex(index);
-  };
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      if (autoPlayInterval === null) {
-        const interval = setInterval(() => {
-          setCurrentSlideIndex((prevIndex) => (prevIndex + 1) % totalSlides);
-        }, 7000);
-        setAutoPlayInterval(interval);
-      }
-    }, 10000);
-
-    return () => clearTimeout(timeout);
-  }, [currentSlideIndex]);
+  const { previousSlide, nextSlide, goToSlide, currentSlideIndex } =
+    useCarousel(5000, totalSlides);
 
   return (
     <div className={styles.carousel}>
       <div className={styles.carousel__slider}>
         <Arrow
           className={styles.carousel__slider_ArrowLeft}
-          onClick={handlePreviousSlide}
+          onClick={previousSlide}
         />
 
         {slidesArray.map((SlideComponent, index) => (
-          <SlideComponent
-            key={index}
-            isActive={currentSlideIndex === index}
-          />
+          <SlideComponent key={index} isActive={currentSlideIndex === index} />
         ))}
 
         <Arrow
           className={styles.carousel__slider_ArrowRight}
-          onClick={handleNextSlide}
+          onClick={nextSlide}
         />
       </div>
 
@@ -89,7 +37,7 @@ export const Сarousel = () => {
                 ? styles.carousel__dots_FullCircle
                 : ""
             }`}
-            onClick={() => handleGoToSlide(index)}
+            onClick={() => goToSlide(index)}
           />
         ))}
       </div>
---
+++ src/services/useCarousel.ts
@@ -0,0 +1,58 @@
+import { useEffect, useRef, useState } from "react";
+
+export default function useCarousel(interval: number, totalSlides: number) {
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+  const [autoPlayInterval, setAutoPlayInterval] = useState<NodeJS.Timeout | null | number>(null);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
+  const previousSlide = () => {
+    pauseAutoPlay();
+    setCurrentSlideIndex(
+      (prevIndex) => (prevIndex - 1 + totalSlides) % totalSlides
+    );
+    setAutoPlayInterval(interval);
+  };
+
+  const nextSlide = () => {
+    pauseAutoPlay();
+    setCurrentSlideIndex((prevIndex) => (prevIndex + 1) % totalSlides);
+    setAutoPlayInterval(interval);
+  };
+
+  const goToSlide = (index: number) => {
+    pauseAutoPlay();
+    setCurrentSlideIndex(index);
+    setAutoPlayInterval(interval);
+  };
+
+  useEffect(() => {
+    intervalRef.current = setInterval(() => {
+      setCurrentSlideIndex((prevIndex) => (prevIndex + 1) % totalSlides);
+    }, interval);
+
+    setAutoPlayInterval(intervalRef.current);
+
+    return () => clearInterval(intervalRef.current!);
+  }, [])
+
+  const pauseAutoPlay = () => {
+    if (autoPlayInterval) {
+      clearInterval(intervalRef.current!);
+      setAutoPlayInterval(null);
+    }
+  };
+
+  useEffect(() => {
+    if (autoPlayInterval !== null) {
+      intervalRef.current = setInterval(() => {
+        setCurrentSlideIndex((prevIndex) => (prevIndex + 1) % totalSlides);
+      }, interval);
+
+      setAutoPlayInterval(intervalRef.current);
+    }
+
+    return () => clearInterval(intervalRef.current!);
+  }, [currentSlideIndex])
+
+  return { previousSlide, nextSlide, goToSlide, currentSlideIndex };
+}
